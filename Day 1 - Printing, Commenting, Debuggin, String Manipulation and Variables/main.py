cf49278f8f794bc6d688199875e74379de5ba05c
Marc Maycas <marc.maycas@gmail.com>
2022-09-20 19:41:31 +0200

    First commit - Day 1 and introductory README.md

---
+++ Day 1 - Printing, Commenting, Debuggin, String Manipulation and Variables/main.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+print("Hello World!\nHello World!\nHello World!") # several lines with \n
+
+print("Hello " + "Marc")
+print("Hello" + " " + "Marc")
+print("Hello", "Marc") # careful with the missing space 
+
+print("Hello " + input("What is your name? ") + "!")
+
+name = input("What's your name? ")
+print("Hello " + name)
+print("Your name is " + str(len(name)) + " characters long")
