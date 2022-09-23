e84071338ce51d9ef9ae1c6dd1ca7e29522d6332
Marc Maycas <marc.maycas@gmail.com>
2022-09-23 14:30:40 +0200

    Fixing leap year bug. Missing condition

--- Day 3 - Conditional Statements, Logical Operators, Code Blocks and Scope/3.3 - Exercise 3 - Leap Year/main.py
+++ Day 3 - Conditional Statements, Logical Operators, Code Blocks and Scope/3.3 - Exercise 3 - Leap Year/main.py
@@ -6,7 +6,10 @@ year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0:
     if year % 100 == 0:
-        leap = False
+        if year % 400 == 0:
+            leap = True
+        else:
+            leap = False
     else:
         leap = True
 else:
--- Day 3 - Conditional Statements, Logical Operators, Code Blocks and Scope/main.py
+++ Day 3 - Conditional Statements, Logical Operators, Code Blocks and Scope/main.py
@@ -29,7 +29,7 @@ if height >=  120:
 else:
     print("Sorry, you have to grow taller before you can ride")
 
-# Example 4 - elif
+# Example 4 - elif (several prices depending on age)
 print("Welcome to the rollercoaster")
 height = int( input("What is your height in cm? ") )
 
@@ -42,5 +42,32 @@ if height >=  120:
         print("Please pay $7.")
     else:
         print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride")
+
+# Example 5 - Multiple if statements
+print("Welcome to the rollercoaster")
+height = int( input("What is your height in cm? ") )
+bill = 0
+
+if height >=  120:
+    print("You can ride the rollercoaster")
+    age = int( input("What is your age? ") )
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age < 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets $12.")
+    
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        # Add $3 to their bill
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
 else:
     print("Sorry, you have to grow taller before you can ride")
