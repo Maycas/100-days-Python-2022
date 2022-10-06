4ebb16b86729192e997bd8981a61408fcefa607d
Marc Maycas <marc.maycas@gmail.com>
2022-10-06 18:24:03 +0200

    Merged encrypt and decrypt in a single function, reducing code repeatability

--- Day 8 - Function parameters and Caesar Cipher/8.3 - Project - Caesar Cipher/main.py
+++ Day 8 - Function parameters and Caesar Cipher/8.3 - Project - Caesar Cipher/main.py
@@ -4,26 +4,17 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+
+    if cipher_direction == 'decode':
+            shift_amount *= (-1)
+
+    for letter in start_text:
         current_position = alphabet.index(letter)
         new_position = (current_position + shift_amount) % len(alphabet)
-        cipher_text += alphabet[new_position]
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
 
-def decrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        current_position = alphabet.index(letter)
-        new_position = (current_position - shift_amount) % len(alphabet)
-        cipher_text += alphabet[new_position]
-    print(f"The decoded text is {cipher_text}")
+    print(f"The {cipher_direction}d text is {end_text}")
 
-
-if direction == "encode":
-    encrypt(plain_text = text, shift_amount = shift)
-elif direction == "decode":
-    decrypt(plain_text = text, shift_amount = shift)
-else:
-    print(f"'{direction}' is not a valid command. Please retry and check your spelling")
+caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
