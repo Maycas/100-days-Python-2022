13871f29ad318367a03d73b2b10fb2024cd57aa1
Marc Maycas <marc.maycas@gmail.com>
2022-10-07 10:34:15 +0200

    Caesar Cypher completed

--- Day 8 - Function parameters and Caesar Cipher/8.3 - Project - Caesar Cipher/main.py
+++ Day 8 - Function parameters and Caesar Cipher/8.3 - Project - Caesar Cipher/main.py
@@ -1,20 +1,35 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import LOGO
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
 
     if cipher_direction == 'decode':
-            shift_amount *= (-1)
+        shift_amount *= (-1)
 
-    for letter in start_text:
-        current_position = alphabet.index(letter)
-        new_position = (current_position + shift_amount) % len(alphabet)
-        end_text += alphabet[new_position]
+    for char in start_text:
+        if char in ALPHABET:
+            current_position = ALPHABET.index(char)
+            new_position = (current_position + shift_amount) % len(ALPHABET)
+            end_text += ALPHABET[new_position]
+        else:
+            end_text += char
 
     print(f"The {cipher_direction}d text is {end_text}")
 
-caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+# Start the Caesar Cypher
+print(LOGO)
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+    result = input("Would you like to continue? Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+    if result == 'no':
+        should_continue = False
+        print("Thanks for using me. Goodbye!")
